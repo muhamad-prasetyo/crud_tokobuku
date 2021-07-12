e42c005b1b05b575ca55938c168234672d31b01d
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-12 19:50:04 +0700

    file koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+<?php 
+// Koneksi ke server mysql 
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "tokobuku";
+
+    $connect = mysqli_connect($server, $username, $password, $database) OR die("Koneksi Gagal");
