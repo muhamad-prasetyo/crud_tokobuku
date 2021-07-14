3a8eba54594fdf604048898c712772bb26f4ada8
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2021-07-14 11:32:22 +0700

    membuat file DeleteSewa.php

--- prosesDeleteSewa.php
+++ prosesDeleteSewa.php
@@ -2,4 +2,13 @@
 
     include_once 'koneksi.php';
 
-    
+    $id = $_GET['id'];
+
+
+   $query = mysqli_query($connect, "DELETE FROM sewa WHERE id ='$id'");
+
+    if(empty($query)) {
+        echo "Data Gagal DiHapus";
+    } else {
+        header("location:index.php");
+    }
